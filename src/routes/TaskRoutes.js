09e7cf8f55433f283a94476af7f4c00ad7dd23c9
mateuslins <41246021+mateuslins@users.noreply.github.com>
2020-07-23 17:17:09 -0300

    Middleware of task validation

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const TaskController = require('../controller/TaskController');
+const TaskValidation = require('../middlewares/TaskValidation');
 
 //Se receber um comando /task, chama a função create do TaskController
 //Como no index ele só é chamado ao receber o comando /task, aqui só precisa deixar um comando /
-router.post('/', TaskController.create);
+//Ao receber o comando respectivo, roda a validação em TaskValidation. Se tudo estiver certo, chama a função de criar na base de dados
+router.post('/', TaskValidation ,TaskController.create);
 
 module.exports = router;
