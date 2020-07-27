2a13413fec8e15fac93d4557864f1bd4ba46418f
mateuslins <41246021+mateuslins@users.noreply.github.com>
2020-07-27 16:18:43 -0300

    Listing tasks by macadress

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -6,8 +6,8 @@ class TaskController {
     async create(req, res) {
         //task vai receber todos os valores de TaskModel, como um objeto
         const task = new TaskModel(req.body);
-        await task
-            .save() //Tenta salvar na base de dados
+
+        await task.save() //Tenta salvar na base de dados
             .then(response => {
                 return res.status(200).json(response);
             }) //Se tudo der certo
@@ -18,6 +18,17 @@ class TaskController {
 
     async update(req, res) {
         await TaskModel.findByIdAndUpdate({ '_id': req.params.id }, req.body, { new: true }) //new true para devolver no response a tarefa atualizada
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            })
+    }
+
+    async all(req, res) {
+        await TaskModel.find({ macaddress: { '$in': req.body.macaddress } })
+            .sort('when')
             .then(response => {
                 return res.status(200).json(response);
             })
--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middlewares/TaskValidation');
+const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
 //Se receber um comando /task, chama a função create do TaskController
 //Como no index ele só é chamado ao receber o comando /task, aqui só precisa deixar um comando /
 //Ao receber o comando respectivo, roda a validação em TaskValidation. Se tudo estiver certo, chama a função de criar na base de dados
 router.post('/', TaskValidation, TaskController.create);
 
-router.put('/:id', TaskController.update);
+router.put('/:id', TaskValidation, TaskController.update);
+
+router.get('/filter/all', MacaddressValidation, TaskController.all);
 
 module.exports = router;
